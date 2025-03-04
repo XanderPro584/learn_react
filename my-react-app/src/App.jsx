09c9069a60b06360fca51d548d9e6f7584152985
XanderPro584 <agrawal.alexander@gmail.com>
2025-03-04 17:13:20 -0600

    Create Student component to use props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,17 @@
 import Card from './Card.jsx';
+import Button from './button.jsx';
+import Student from './Student.jsx';
 
 function App() {
 
   return(
     <>
-      <Card />
+      <Student name="Spongebob" age="20" isStudent={true}/>
+      <Student name="Patrick" age={42} isStudent={false}/>
+      <Student name="Squidward" age={50} isStudent={false}/>
+      <Student name="Sandy" age={27} isStudent={true}/>
+      <Student/>
+      
     </>
   )
 }
